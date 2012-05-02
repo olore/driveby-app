8d6723f4163f767a6652dda3286c65b560f710d3
Brian Olore <brian@olore.net>
2012-05-02 00:21:10 -0400

    license plate images!

--- public/app/js/driveby.js
+++ public/app/js/driveby.js
@@ -7,11 +7,16 @@ DriveBy.states = [
  'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 
  'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY'];
 
+Handlebars.registerHelper('toLowerCase', function(value) {
+  if (value == null) return 'nj';
+  return new Handlebars.SafeString(value.toLowerCase());
+});
+
 DriveBy.add_recent_posts_to = function( recent_list ) {
 
   $.get( "/posts", function( posts ) {
     //TODO: move this out somewhere
-    var post_source = "<li><p title='{{post.created_at}}' class='timeago ui-li-aside'>{{post.created_at}}</p><h3>{{post.state}} {{post.license_plate}}</h3><p>{{post.comment}}</p></li>";
+    var post_source = "<li><div style='position: relative; background: url(/app/images/{{toLowerCase post.state}}.jpg) no-repeat; width: 250px; height: 130px;'/><p title='{{post.created_at}}' class='timeago ui-li-aside'>{{post.created_at}}</p><div style='position: absolute; top: 0; left: 20px; width: 240px'><h3 style='font-size: 40px; position: relative; top: 30px; text-align: center;'>{{post.state}} {{post.license_plate}}</h3></div><p>{{post.comment}}</p></li>";
     var post_template = Handlebars.compile(post_source);
 
     $.each( posts , function(index, post) {
@@ -27,7 +32,7 @@ DriveBy.initialize = function() {
   
   //add states
   $.each( DriveBy.states , function(index, state) {
-    $( '.slider' ).append('<div class="item" id ="' + state + '"><a href="#">' + state + '</a></div>');
+    $( '.slider' ).append('<div class="item" id ="' + state + '"><a href="#"><img width="120" height="60" src="/app/images/' + state.toLowerCase() + '.jpg" /></a></div>');
   });
 
   //log any ajax errors
