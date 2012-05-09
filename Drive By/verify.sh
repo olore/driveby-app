f12d249a53febc3cb7325f93b82bac5924a0eb5f
Brian Olore <brian@olore.net>
2012-05-08 21:26:28 -0400

    symlinks don't work, copy stuff over during build

--- Drive By/verify.sh
+++ Drive By/verify.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "start"
 
 if [ ! -d "$PROJECT_DIR/www" ] ; then
 	cp -R /Users/Shared/Cordova/Frameworks/Cordova.framework/www "$PROJECT_DIR"
@@ -10,3 +11,8 @@ rc=$?
 if [ $rc != 0 ] ; then
 echo -e "warning: Missing - Add $PROJECT_DIR/www as a folder reference in your project. Just drag and drop the folder into your project, into the Project Navigator of Xcode 4. Make sure you select the second radio-button: 'Create folder references for any added folders' (which will create a blue folder)" 1>&2
 fi
+echo "hi"
+rm -rf "$PROJECT_DIR/www/*"
+cp /Users/brian/dev/ruby/driveby.olore.net/public/app/index.html "$PROJECT_DIR/www"
+
+cp -r /Users/brian/dev/ruby/driveby.olore.net/public/app "$PROJECT_DIR/www"
