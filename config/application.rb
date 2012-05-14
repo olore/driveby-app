be99f47f5308f3bb9c8cce7e0d2479b9624eabfc
Brian Olore <brian@olore.net>
2012-05-13 22:50:45 -0400

    stuff for help page

--- config/application.rb
+++ config/application.rb
@@ -5,14 +5,14 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module Driveby
@@ -56,5 +56,6 @@ module Driveby
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = true
 
+    config.assets.enabled = true
   end
 end
